3242f9006a183ff7df7111024e8492f80db6f150
tatarynovdima <dmytro.course@gmail.com>
2023-03-05 20:53:20 +0100

    Initial commit

---
+++ src/main/java/com/example/workspace/TkpCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkpCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkpCourseWorkApplication.class, args);
+    }
+
+}
